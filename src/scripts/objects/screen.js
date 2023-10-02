7d680a25eb2fd7339fbce561e445b64ea8cdf6e2
Jessica Oliveira <elisa0js@gmail.com>
2023-10-02 15:27:39 -0300

    refatoração código JS

--- src/scripts/objects/screen.js
+++ src/scripts/objects/screen.js
@@ -1,64 +1,67 @@
+import { baseUrl, repositoriesQuantity } from "/src/scripts/variables.js";
+import { getEvents } from "/src/scripts/services/events.js";
+
 const screen = {
   userProfile: document.querySelector(".profile-data"),
   renderUser(user) {
     this.userProfile.innerHTML = `<div class= "info">
                                     <img src="${user.avatarUrl}" alt:"Foto do perfil do usuário"/>
                                     <div class= "data">
-                                      <h1>${user.name ?? "Este usuário nao possui nome cadastrado"}</h1>
-                                      <p>${user.bio ?? "Este usuário nao possui bio cadastrada"}</p>
-                                      <p class= "followers">${`Seguidores: ${user.followers} 👥`}</p>
-                                      <p>${`Seguindo: ${user.following} 👣`}</p>
+                                        <h1>${ user.name ??"Este usuário nao possui nome cadastrado" }</h1>
+                                        <p>${ user.bio ?? "Este usuário nao possui bio cadastrada"  }</p>
+                                        <p class= "followers">${`Seguidores: ${user.followers} 👥`}</p>
+                                        <p>${`Seguindo: ${user.following} 👣`}</p>
                                     </div>
                                   </div>`;
-
-let repositoriesItens = user.repositories.map((repo) =>
+ 
+    let repositoriesItens = user.repositories
+      .map(
+        (repo) =>
           `<li>
               <div class="info-repositories"> 
-                <a href="${repo.html_url}" target="_blank"><span>${repo.name}</span>
-                <div class="data-repositories">
+                <a href="${repo.html_url}" target="_blank"><span>${ repo.name }</span>
+              <div class="data-repositories">
                   <ul>
                     <li>🍴${repo.forks}</li>
                     <li>⭐${repo.stargazers_count}</li>
                     <li>👀${repo.watchers}</li>
                     <li>👨‍💻${repo.language ?? "linguagem indefinida"}</li>
                   </ul>     
-                </div> 
+              </div> 
                 </a>   
               </div>  
            </li>`
-          ).join("");
-     
-this.userProfile.innerHTML += `<div class="repositories">
-                                  <h2>Repositórios</h2>
-                                  <ul>${repositoriesItens}</ul>
-                              <div>`;
-
-const validEvents = user.events.filter((event) => 
-                    event.type === "PushEvent" || event.type === "CreateEvent" );
-    if (validEvents.length === 0) {
-      this.userProfile.innerHTML += `<div class="events">
-                                        <p>Não possui eventos registrados</p>          
-                                     </div>`;
-                                      return;
-                                  }
-
-const itemEvent = validEvents.map((event) => {
-          if (event.type === "CreateEvent") {
-              return `<li><span>${event.repo.name}</span> - Create: ${event.payload.ref_type}</li>`;
-        } else if (event.type === "PushEvent") {
-              return `<li><span>${event.repo.name}</span> - ${event.payload.commits[0].message}</li>`;
-        } else !event.type === "CreateEvent" || !event.type === "PushEvent"; {
-              return
-        }
-      })
+      )
       .join("");
-      
+
+    this.userProfile.innerHTML += `<div class="repositories">
+                                    <h2>Repositórios</h2>
+                                    <ul>${repositoriesItens}</ul>
+                                  <div>`;
+
+    let eventItens = "";
+    user.events.forEach((element) => {
+      if (element.type === "PushEvent") {
+        eventItens += `<li> 
+                         <h3>${element.repo.name}</h3>
+                         <p>-- ${element.payload.commits[0].message}</p>
+                       </li>
+            `;
+      } else {
+        eventItens += `<li> 
+                        <h3>${element.repo.name}</h3>
+                        <p>-- Criado um ${element.payload.ref_type}</p>
+                       </li>`;
+      }
+    });
 
     if (user.events.length > 0) {
       this.userProfile.innerHTML += `<div class="events">
-                                         <h2>Eventos</h2>
-                                         <ul>${itemEvent}</ul>
+                                          <h2>Eventos</h2>
+                                          <ul>${eventItens}</ul>
                                      </div>`;
+    }else{
+      this.userProfile.innerHTML+="<h3>Este usuário não possui eventos criados.</h3>"
     }
   },
   renderNotFound() {
--- src/scripts/services/events.js
+++ src/scripts/services/events.js
@@ -3,11 +3,15 @@ import { baseUrl, repositoriesQuantity } from "/src/scripts/variables.js";
 async function getEvents(userName) {
   const response = await fetch(
     `${baseUrl}/${userName}/events?per_page=${repositoriesQuantity}`
-    
-  ); 
+  );
+  const events = await response.json();
   
-return await response.json();
-
-  
+  return events
+    .filter(
+      (element) =>
+        element.type === "CreateEvent" || element.type === "PushEvent"
+    )  
+    .slice(0, repositoriesQuantity);
+   
 }
-export {getEvents};
+export { getEvents };
